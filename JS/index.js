85017c6eb3acb315e2689a58b70012dea96d24e3
P156HAM <a.hamdi.almasalmeh@gmail.com>
2022-11-22 23:06:03 +0100

    update

--- JS/index.js
+++ JS/index.js
@@ -9,17 +9,42 @@ async function getTarget() {
 
     console.log('answer from API', data)
     displayTarget(data)
+    displaySubTarget(data)
 }
 getTarget()
-
-const targets = getTarget;
 
 function displayTarget(data) {
     const displaySpace = document.querySelector('.display-space')
     let targets = '';
     for (let i=0; i<data.length; i++) {
-        targets += `<div class='targets'> ${data[i].title} </div>` 
+        targets += `<div class='targets'> ${data[i].title} </div>
+                    <button class='button${i}'> Show more </button>` 
     }
+
+    const goalCode = data.map((target) => {
+        return target.code;
+    })
+    console.log(goalCode)
+
     displaySpace.innerHTML = targets;
+    let btn = document.querySelectorAll('button'); 
+    btn = document.addEventListener('click', () => {
+        btn.map((button) => {
+            
+        })
+    })
+}
+
+async function displaySubTarget(data) {
+    const baseUrl = 'https://unstats.un.org/SDGAPI/v1/sdg/Goal/{goalCode}/Target/List?includechildren=true'
+    const goalCode = data.map((target) => {
+        return target.code;
+    })
+    console.log(goalCode)
+
+    const url = `https://unstats.un.org/SDGAPI/v1/sdg/Goal/${goalCode}/Target/List?includechildren=true`
+    const response = await fetch(url); 
+    const log = await response.json(); 
 
 }
+
