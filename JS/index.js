e66932c6e2b05ae38d1c1a8d13cee4fdb46a66c2
P156HAM <a.hamdi.almasalmeh@gmail.com>
2022-11-24 13:49:53 +0100

    update

--- JS/index.js
+++ JS/index.js
@@ -3,13 +3,13 @@
 
 
 async function getTarget() {
-    const baseUrl = 'https://unstats.un.org/SDGAPI/v1/sdg/Goal/List?includechildren=true'
+    const baseUrl = 'https://unstats.un.org/SDGAPI/v1/sdg/Goal/List?includechildren=false'
     const response = await fetch(baseUrl); 
     const data = await response.json();
 
     console.log('answer from API', data)
     displayTarget(data)
-    displaySubTarget(data)
+    getEveryGoalCode(data)
 }
 getTarget()
 
@@ -18,33 +18,47 @@ function displayTarget(data) {
     let targets = '';
     for (let i=0; i<data.length; i++) {
         targets += `<div class='targets'> ${data[i].title} </div>
-                    <button class='button${i}'> Show more </button>` 
+                    <button data-goal='${data[i].code}' class='button${i}'> Show more </button>` 
     }
 
-    const goalCode = data.map((target) => {
-        return target.code;
-    })
-    console.log(goalCode)
-
     displaySpace.innerHTML = targets;
-    let btn = document.querySelectorAll('button'); 
-    btn = document.addEventListener('click', () => {
-        btn.map((button) => {
-            
-        })
-    })
+    
 }
 
-async function displaySubTarget(data) {
-    const baseUrl = 'https://unstats.un.org/SDGAPI/v1/sdg/Goal/{goalCode}/Target/List?includechildren=true'
+async function getEveryGoalCode(data) {
     const goalCode = data.map((target) => {
         return target.code;
     })
-    console.log(goalCode)
+    for (let x=0; x<goalCode.length; x++) {
+    const url = `https://unstats.un.org/SDGAPI/v1/sdg/Goal/${goalCode[x]}/Target/List?includechildren=true`
+    const response = await fetch(url);
+    const targetData = await response.json();
 
-    const url = `https://unstats.un.org/SDGAPI/v1/sdg/Goal/${goalCode}/Target/List?includechildren=true`
-    const response = await fetch(url); 
-    const log = await response.json(); 
-
+    return targetData;
+    }
+    displaySubTarget()
+    
 }
+
+function displaySubTarget() {
+    
+    let btns = document.querySelectorAll('button');
+    let area = document.querySelector('.display-area');
+    btns.forEach((button) => {
+        button.addEventListener('click', async () => {
+            let code = button.getAttribute('data-goal')
+            let response = await fetch(`https://unstats.un.org/SDGAPI/v1/sdg/Goal/${code}/Target/List?includechildren=true`)
+            let targetData = await response.json(); 
+            console.log(targetData[0].targets);
+            for (let i=0; i<targetData[0].targets.length; i++) {
+                let displayArea = document.createElement('div');
+                displayArea.innerHTML = targetData[0].targets[i].title;
+                area.append(displayArea);
+            }
+        })
+    });
+}
+
+
+
 
